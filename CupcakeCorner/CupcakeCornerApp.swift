bf7cdefee3e7520b857fdd7455a4f52e78edfb27
Lenya <leonidivanov826@gmail.com>
2026-06-16 23:02:16 +0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Леонід Іванов on 16.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
